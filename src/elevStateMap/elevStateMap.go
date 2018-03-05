cbcd789ff4c94cf12feff2e505911850e74d4fc0
juliehaga <jul.haga@gmail.com>
2018-03-05 14:31:18 +0100

    test config

--- src/elevStateMap/elevStateMap.go
+++ src/elevStateMap/elevStateMap.go
@@ -83,6 +83,8 @@ func UpdateLocalMap(changedMap ElevStateMap){
 		}
 	}
 }
+}
+
 
 func UpdateMapFromNetwork(recievedMap ElevStateMap, newOrderChan chan elevio.ButtonEvent){
 	for e:= 0; e < config.NUM_ELEVS; e++{
@@ -98,7 +100,7 @@ func UpdateMapFromNetwork(recievedMap ElevStateMap, newOrderChan chan elevio.But
 				for b:= elevio.BT_HallUp; b <= elevio.BT_Cab; b++{
 					if recievedMap[e].Orders[f][b] == OT_OrderPlaced && LocalMap[e].Orders[f][b] == OT_NoOrder{
 						newOrderChan <- elevio.ButtonEvent{f, b}
-						//fmt.Printf("BUTTONEVENT FROM NETWORK\n\n")
+						fmt.Printf("BUTTONEVENT FROM NETWORK\n\n")
 					}
 					LocalMap[e].Orders[f][b] = recievedMap[e].Orders[f][b]
 				}
