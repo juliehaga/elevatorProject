bb7eb8c134132671f2404a5197abc0416f4d7d1c
Johan Brun <johannicbrun@gmail.com>
2018-04-11 15:38:01 +0200

    door open

--- src/elevStateMap/elevStateMap.go
+++ src/elevStateMap/elevStateMap.go
@@ -210,7 +210,7 @@ func UpdateMapFromNetwork(recievedMap config.ElevStateMap, newOrderChan chan con
 		for f:= 0; f < config.NUM_FLOORS; f++{
 			for b:= config.BT_HallUp; b < config.BT_Cab; b++{
 
-			if recievedMap[config.My_ID].Orders[f][b] ==config.OT_NoOrder && currentMap[config.My_ID].Orders[f][b] == config.OT_OrderPlaced){
+			if recievedMap[config.My_ID].Orders[f][b] ==config.OT_NoOrder && currentMap[config.My_ID].Orders[f][b] == config.OT_OrderPlaced{
 
 
 					for e := 0; e < config.NUM_ELEVS; e++{
