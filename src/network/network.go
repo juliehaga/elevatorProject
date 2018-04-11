09ac56292a0a6e972793eed66e48bc49c7f42ea3
emilhThyri <emilThyri@gmail.com>
2018-04-11 11:13:38 +0200

    test

--- src/network/network.go
+++ src/network/network.go
@@ -143,21 +143,21 @@ func Transmitter(port int, messageTx chan config.Message, ackChan chan config.Ac
 			case message := <- messageTx:
 				
 
-				addr, _ := net.ResolveUDPAddr("udp", fmt.Sprintf("255.255.255.255:%d", port))
-				conn, _ := net.DialUDP("udp", nil, addr)
+				//addr, _ := net.ResolveUDPAddr("udp", fmt.Sprintf("255.255.255.255:%d", port))
+				//conn, _ := net.DialUDP("udp", nil, addr)
 				
 
-				//for e:= 0; e < config.NUM_ELEVS; e++{
+				for e:= 0; e < config.NUM_ELEVS; e++{
 
 
-				//	if e != config.My_ID{
-				//		message.Reciever_ID = e
-						buf, _ := json.Marshal(message)
+					if e != config.My_ID{
+						message.Reciever_ID = e
+				//		buf, _ := json.Marshal(message)
 						//fmt.Printf("heis %v er connected %v\n", e, message.ElevMap[e].Connected)
-				//		if message.ElevMap[e].Connected == true{
+						if message.ElevMap[e].Connected == true{
 							//fmt.Printf("Ny melding\n")
-							conn.Write(buf) //DEnne skal ikke være her når vi har med ack
-				/*			
+							//conn.Write(buf) //DEnne skal ikke være her når vi har med ack
+							
 							WAIT_FOR_ACK:
 								for i := 0; i < 5; i++{
 									sendUdpMsg(message, port)
@@ -172,10 +172,10 @@ func Transmitter(port int, messageTx chan config.Message, ackChan chan config.Ac
 									}
 									
 									//antar at peer vil fiksa å sette til dead dersom en faller ut.
-							}*/
-				//	}
-			//	}
-			//}
+							}
+					}
+				}
+			}
 
 		time.Sleep(2* time.Millisecond)
 		}	
