4c1cdf834bade2daf3308e293d698542c63cd81d
aalexjo <alexanjohansen@gmail.com>
2018-04-12 13:11:11 +0200

    før endring av nettverksmodul

--- src/network/network.go
+++ src/network/network.go
@@ -219,6 +219,67 @@ func Receiver(port int, orderMsgRx chan config.OrderMsg, statusMsgRx chan config
 }
 
 
+// Checks that args to Tx'er/Rx'er are valid:
+//  All args must be channels
+//  Element types of channels must be encodable with JSON
+//  No element types are repeated
+// Implementation note:
+//  - Why there is no `isMarshalable()` function in encoding/json is a mystery,
+//    so the tests on element type are hand-copied from `encoding/json/encode.go`
+func checkArgs(chans ...interface{}) {
+	n := 0
+	for range chans {
+		n++
+	}
+	elemTypes := make([]reflect.Type, n)
+
+	for i, ch := range chans {
+		// Must be a channel
+		if reflect.ValueOf(ch).Kind() != reflect.Chan {
+			panic(fmt.Sprintf(
+				"Argument must be a channel, got '%s' instead (arg#%d)",
+				reflect.TypeOf(ch).String(), i+1))
+		}
+
+		elemType := reflect.TypeOf(ch).Elem()
+
+		// Element type must not be repeated
+		for j, e := range elemTypes {
+			if e == elemType {
+				panic(fmt.Sprintf(
+					"All channels must have mutually different element types, arg#%d and arg#%d both have element type '%s'",
+					j+1, i+1, e.String()))
+			}
+		}
+		elemTypes[i] = elemType
+
+		// Element type must be encodable with JSON
+		switch elemType.Kind() {
+		case reflect.Complex64, reflect.Complex128, reflect.Chan, reflect.Func, reflect.UnsafePointer:
+			panic(fmt.Sprintf(
+				"Channel element type must be supported by JSON, got '%s' instead (arg#%d)",
+				elemType.String(), i+1))
+		case reflect.Map:
+			if elemType.Key().Kind() != reflect.String {
+				panic(fmt.Sprintf(
+					"Channel element type must be supported by JSON, got '%s' instead (map keys must be 'string') (arg#%d)",
+					elemType.String(), i+1))
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 func checkArgs(chans ...interface{}) {
 	n := 0
